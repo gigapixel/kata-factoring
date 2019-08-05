6f5ba900da1207e66c1fa0e9a40693ef258cf970
Chaiwat Chaichan <gusjib.j@gmail.com>
2019-08-05 11:31:15 +0700

    factoring beta

--- main.go
+++ main.go
@@ -4,4 +4,19 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello")
+	primeFactor(20)
+}
+
+func primeFactor(num int) {
+	fmt.Println("input ==> ", num)
+	p := 2
+	for ok := true; ok; ok = (num >= (p * p)) {
+    if ((num % p) == 0) {
+			fmt.Println("p = ", p)
+			num = num / p
+		} else {
+			p = p + 1
+		}
+	}
+	fmt.Println("num = ", num)
 }
