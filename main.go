5771fc51dde7e29f00ae2818e374bfe9324e141d
Chaiwat Chaichan <gusjib.j@gmail.com>
2019-08-06 09:24:44 +0700

    find prime

--- main.go
+++ main.go
@@ -3,20 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
 	primeFactor(20)
 }
 
 func primeFactor(num int) {
 	fmt.Println("input ==> ", num)
 	p := 2
+	fmt.Print("Prime ==> ")
 	for ok := true; ok; ok = (num >= (p * p)) {
     if ((num % p) == 0) {
-			fmt.Println("p = ", p)
+			fmt.Print(p)
+			fmt.Print(" * ")
 			num = num / p
 		} else {
 			p = p + 1
 		}
 	}
-	fmt.Println("num = ", num)
+	fmt.Println(num)
 }
