0404ed00175eff61cff707ee827d2acb0045ebf0
babellow <kawvalin@dek-d.com>
2019-08-07 11:56:02 +0700

    get prime

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
 import "fmt"
+import "strconv"
+import "strings"
 
 func main() {
-	fmt.Println("Hello")
+	input := 11
+	var ints []string
+	index := 2
+	for input > 0 {
+		if(index > input) { break };
+		if input % index == 0 {
+			input = input / index
+			ints = append(ints, strconv.Itoa(index));
+		} else {
+			index++
+		}
+	}
+	fmt.Println("result => ", strings.Join(ints[:], " x "))
 }
