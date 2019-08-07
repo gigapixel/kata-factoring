7dc710f5823f7f434a1b33acfc566d2cf96f7bd6
bmqdev <bmqdev@gmail.com>
2019-08-07 11:55:29 +0700

    prime

--- main.go
+++ main.go
@@ -3,5 +3,57 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
+	printFactor(12)
+	// fmt.Println("Hello");
 }
+
+func printFactor(num int){
+	var factors []int
+	primes := [...] int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 221, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 403, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 481, 487, 491, 499, 503, 509, 521, 523, 533, 541, 547, 559, 563, 569, 571, 577, 587, 593, 599, 601, 607, 611, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673,  677, 683, 689, 691, 701, 709, 719, 727, 733, 739, 743, 751, 767, 769, 773, 787, 793, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 871, 877, 881, 883, 887, 907, 911, 919, 923, 929, 937, 941, 947, 949, 953, 967, 971, 977, 983, 991, 997}
+	// var a = prime(num);
+	// fmt.Println("----->", a);
+
+	for num != 1 {
+		for _, prime := range primes {
+			var modResult = num % prime;
+			if(modResult == 0){
+				factors = append(factors, prime)
+				num = num / prime;
+			}
+		}
+	}
+	fmt.Println(factors)
+}
+
+func prime(N int) (primes []int) {
+	b := make([]bool, N)
+	for i := 2; i < N; i++ {
+			if b[i] == true { continue }
+			primes = append(primes, i)
+			for k := i * i; k < N; k += i {
+					b[k] = true
+			}
+	}
+	return
+}
+
+// func isPrime(num int) bool{
+
+// }
+
+// func prime(num int) int{
+// 	var res []int
+// 	i := 1;
+// 	for i < num {
+// 		// x := 1 
+// 		// for x < num {
+// 		// 	fmt.Println("------", x);
+// 		// 	x += 1;
+// 		// }
+// 		fmt.Println(i);
+// 		i += 1;
+// 	}
+
+// 	fmt.Println(res, num);
+// 	return num;
+// }
